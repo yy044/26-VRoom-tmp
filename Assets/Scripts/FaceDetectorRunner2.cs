595ccc2f332723a115f3632ba711e0a92929953b
asimov <206011497+asimov-hy@users.noreply.github.com>
2026-05-09 00:07:11 +0900

    Setup MediaPipe desktop prototype

---
+++ Assets/Scripts/FaceDetectorRunner2.cs
@@ -0,0 +1,225 @@
+// Copyright (c) 2023 homuler
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+using System.Collections;
+using Mediapipe.Tasks.Vision.FaceDetector;
+using UnityEngine;
+using UnityEngine.Rendering;
+using FaceDetectionResult = Mediapipe.Tasks.Components.Containers.DetectionResult;
+
+namespace Mediapipe.Unity.Sample.FaceDetection
+{
+  public class FaceDetectorRunner2 : VisionTaskApiRunner<FaceDetector>
+  {
+    [SerializeField] private DetectionResultAnnotationController _detectionResultAnnotationController;
+
+    private Experimental.TextureFramePool _textureFramePool;
+
+    private readonly object _latestResultLock = new object();
+    private FaceDetectionResult _latestResult;
+    private bool _hasLatestResult;
+
+    public readonly FaceDetectionConfig config = new FaceDetectionConfig();
+
+    public int ImageWidth { get; private set; }
+    public int ImageHeight { get; private set; }
+
+    public bool TryGetLatestResult(ref FaceDetectionResult result)
+    {
+      lock (_latestResultLock)
+      {
+        if (!_hasLatestResult || _latestResult.detections == null || _latestResult.detections.Count == 0)
+        {
+          return false;
+        }
+
+        _latestResult.CloneTo(ref result);
+        return true;
+      }
+    }
+
+    private void SaveLatestResult(FaceDetectionResult result)
+    {
+      lock (_latestResultLock)
+      {
+        _hasLatestResult = result.detections != null && result.detections.Count > 0;
+
+        if (_hasLatestResult)
+        {
+          result.CloneTo(ref _latestResult);
+        }
+        else
+        {
+          _latestResult = default;
+        }
+      }
+    }
+
+    private void ClearLatestResult()
+    {
+      lock (_latestResultLock)
+      {
+        _hasLatestResult = false;
+        _latestResult = default;
+      }
+    }
+
+    public override void Stop()
+    {
+      base.Stop();
+      _textureFramePool?.Dispose();
+      _textureFramePool = null;
+      ImageWidth = 0;
+      ImageHeight = 0;
+      ClearLatestResult();
+    }
+
+    protected override IEnumerator Run()
+    {
+      Debug.Log($"Delegate = {config.Delegate}");
+      Debug.Log($"Image Read Mode = {config.ImageReadMode}");
+      Debug.Log($"Model = {config.ModelName}");
+      Debug.Log($"Running Mode = {config.RunningMode}");
+      Debug.Log($"MinDetectionConfidence = {config.MinDetectionConfidence}");
+      Debug.Log($"MinSuppressionThreshold = {config.MinSuppressionThreshold}");
+      Debug.Log($"NumFaces = {config.NumFaces}");
+
+      yield return AssetLoader.PrepareAssetAsync(config.ModelPath);
+
+      var options = config.GetFaceDetectorOptions(config.RunningMode == Tasks.Vision.Core.RunningMode.LIVE_STREAM ? OnFaceDetectionsOutput : null);
+      taskApi = FaceDetector.CreateFromOptions(options, GpuManager.GpuResources);
+      var imageSource = ImageSourceProvider.ImageSource;
+
+      yield return imageSource.Play();
+
+      if (!imageSource.isPrepared)
+      {
+        Debug.LogError("Failed to start ImageSource, exiting...");
+        yield break;
+      }
+
+      ImageWidth = imageSource.textureWidth;
+      ImageHeight = imageSource.textureHeight;
+      ClearLatestResult();
+
+      // Use RGBA32 as the input format.
+      // TODO: When using GpuBuffer, MediaPipe assumes that the input format is BGRA, so maybe the following code needs to be fixed.
+      _textureFramePool = new Experimental.TextureFramePool(imageSource.textureWidth, imageSource.textureHeight, TextureFormat.RGBA32, 10);
+
+      // NOTE: The screen will be resized later, keeping the aspect ratio.
+      screen.Initialize(imageSource);
+
+      if (_detectionResultAnnotationController != null)
+      {
+          SetupAnnotationController(_detectionResultAnnotationController, imageSource);
+      }
+
+      var transformationOptions = imageSource.GetTransformationOptions();
+      var flipHorizontally = transformationOptions.flipHorizontally;
+      var flipVertically = transformationOptions.flipVertically;
+      var imageProcessingOptions = new Tasks.Vision.Core.ImageProcessingOptions(rotationDegrees: (int)transformationOptions.rotationAngle);
+
+      AsyncGPUReadbackRequest req = default;
+      var waitUntilReqDone = new WaitUntil(() => req.done);
+      var waitForEndOfFrame = new WaitForEndOfFrame();
+      var result = FaceDetectionResult.Alloc(options.numFaces);
+
+      // NOTE: we can share the GL context of the render thread with MediaPipe (for now, only on Android)
+      var canUseGpuImage = SystemInfo.graphicsDeviceType == GraphicsDeviceType.OpenGLES3 && GpuManager.GpuResources != null;
+      using var glContext = canUseGpuImage ? GpuManager.GetGlContext() : null;
+
+      while (true)
+      {
+        if (isPaused)
+        {
+          yield return new WaitWhile(() => isPaused);
+        }
+
+        if (!_textureFramePool.TryGetTextureFrame(out var textureFrame))
+        {
+          yield return null;
+          continue;
+        }
+
+        // Build the input Image
+        Image image;
+        switch (config.ImageReadMode)
+        {
+          case ImageReadMode.GPU:
+            if (!canUseGpuImage)
+            {
+              throw new System.Exception("ImageReadMode.GPU is not supported");
+            }
+            textureFrame.ReadTextureOnGPU(imageSource.GetCurrentTexture(), flipHorizontally, flipVertically);
+            image = textureFrame.BuildGPUImage(glContext);
+            // TODO: Currently we wait here for one frame to make sure the texture is fully copied to the TextureFrame before sending it to MediaPipe.
+            // This usually works but is not guaranteed. Find a proper way to do this. See: https://github.com/homuler/MediaPipeUnityPlugin/pull/1311
+            yield return waitForEndOfFrame;
+            break;
+          case ImageReadMode.CPU:
+            yield return waitForEndOfFrame;
+            textureFrame.ReadTextureOnCPU(imageSource.GetCurrentTexture(), flipHorizontally, flipVertically);
+            image = textureFrame.BuildCPUImage();
+            textureFrame.Release();
+            break;
+          case ImageReadMode.CPUAsync:
+          default:
+            req = textureFrame.ReadTextureAsync(imageSource.GetCurrentTexture(), flipHorizontally, flipVertically);
+            yield return waitUntilReqDone;
+
+            if (req.hasError)
+            {
+              Debug.LogWarning($"Failed to read texture from the image source");
+              textureFrame.Release();
+              continue;
+            }
+            image = textureFrame.BuildCPUImage();
+            textureFrame.Release();
+            break;
+        }
+
+        switch (taskApi.runningMode)
+        {
+          case Tasks.Vision.Core.RunningMode.IMAGE:
+            if (taskApi.TryDetect(image, imageProcessingOptions, ref result))
+            {
+              _detectionResultAnnotationController?.DrawNow(result);
+              SaveLatestResult(result);
+            }
+            else
+            {
+              // clear the annotation
+              _detectionResultAnnotationController?.DrawNow(default);
+              ClearLatestResult();
+            }
+            break;
+          case Tasks.Vision.Core.RunningMode.VIDEO:
+            if (taskApi.TryDetectForVideo(image, GetCurrentTimestampMillisec(), imageProcessingOptions, ref result))
+            {
+              _detectionResultAnnotationController?.DrawNow(result);
+              SaveLatestResult(result);
+            }
+            else
+            {
+              // clear the annotation
+              _detectionResultAnnotationController?.DrawNow(default);
+              ClearLatestResult();
+            }
+            break;
+          case Tasks.Vision.Core.RunningMode.LIVE_STREAM:
+            taskApi.DetectAsync(image, GetCurrentTimestampMillisec(), imageProcessingOptions);
+            break;
+        }
+      }
+    }
+
+    private void OnFaceDetectionsOutput(FaceDetectionResult result, Image image, long timestamp)
+    {
+      _detectionResultAnnotationController?.DrawLater(result);
+      SaveLatestResult(result);
+    }
+  }
+}
--- Assets/Scripts/HeadTracker.cs
+++ Assets/Scripts/HeadTracker.cs
@@ -1,148 +1,73 @@
-using System.Collections;
-using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
-using Mediapipe;
-using Mediapipe.Unity;
-using Mediapipe.Tasks.Vision.FaceDetector;
+using UnityEngine;
+using Mediapipe.Unity.Sample.FaceDetection;
 using FaceDetectionResult = Mediapipe.Tasks.Components.Containers.DetectionResult;
 
 public class HeadTracker : MonoBehaviour
 {
     [Header("References")]
-    public RawImage webcamDisplay;
+    public FaceDetectorRunner2 runner;
+    public RectTransform displayRect;
     public RectTransform headLabel;
     public TextMeshProUGUI headLabelText;
-    public Canvas canvas;
 
-    [Header("Settings")]
-    public float minConfidence = 0.5f;
-    [Range(0.01f, 0.15f)] public float labelOffsetAboveHead = 0.08f;
+    [Header("Label")]
+    public string labelText = "Hello!";
+    public float yOffset = 40f;
+    public bool mirrorX = false;
+    public float smoothing = 15f;
 
-    private WebCamTexture _webcam;
-    private FaceDetector _detector;
     private FaceDetectionResult _result;
-    private Texture2D _cpuTexture;
-    private Color32[] _pixels;
-    private bool _ready;
 
-    IEnumerator Start()
+    void Start()
     {
-        headLabel.gameObject.SetActive(false);
-
-        // Start webcam
-        _webcam = new WebCamTexture(1280, 720, 30);
-        webcamDisplay.texture = _webcam;
-        _webcam.Play();
-        webcamDisplay.rectTransform.localScale = new Vector3(-1, 1, 1);
-
-        while (_webcam.width < 100)
-            yield return null;
-
-        Debug.Log("Webcam started: " + _webcam.width + "x" + _webcam.height);
-
-        // Prepare model asset
-        yield return AssetLoader.PrepareAssetAsync("blaze_face_short_range.bytes");
-
-        // Create detector
-        var baseOptions = new Mediapipe.Tasks.Core.BaseOptions(
-            Mediapipe.Tasks.Core.BaseOptions.Delegate.CPU,
-            modelAssetPath: "blaze_face_short_range.bytes"
-        );
-
-        var options = new FaceDetectorOptions(
-            baseOptions,
-            runningMode: Mediapipe.Tasks.Vision.Core.RunningMode.IMAGE,
-            numFaces: 1,
-            minDetectionConfidence: minConfidence,
-            minSuppressionThreshold: 0.3f
-        );
-
-        _detector = FaceDetector.CreateFromOptions(options);
-        _result = FaceDetectionResult.Alloc(1);
-
-        _cpuTexture = new Texture2D(_webcam.width, _webcam.height, TextureFormat.RGBA32, false);
-        _pixels = new Color32[_webcam.width * _webcam.height];
-
-        _ready = true;
-        Debug.Log("Face detector ready");
+        if (headLabel != null)
+            headLabel.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        if (!_ready || !_webcam.didUpdateThisFrame)
+        if (runner == null || displayRect == null || headLabel == null)
             return;
 
-        _webcam.GetPixels32(_pixels);
-        _cpuTexture.SetPixels32(_pixels);
-        _cpuTexture.Apply();
-
-        byte[] rawData = _cpuTexture.GetRawTextureData();
-
-        var imageFrame = new ImageFrame(
-            ImageFormat.Types.Format.Srgba,
-            _webcam.width,
-            _webcam.height,
-            _webcam.width * 4,
-            rawData
-        );
-
-        var image = new Mediapipe.Image(imageFrame);
-
-        if (_detector.TryDetect(image, null, ref _result) && _result.detections != null && _result.detections.Count > 0)
-        {
-            var detection = _result.detections[0];
-            Debug.Log("Detection found. Keypoints: " + (detection.keypoints != null ? detection.keypoints.Count.ToString() : "null"));
-            Debug.Log("BoundingBox: " + detection.boundingBox);
-
-            PositionLabel(detection);
-            headLabel.gameObject.SetActive(true);
-        }
-        else
+        if (!runner.TryGetLatestResult(ref _result))
         {
             headLabel.gameObject.SetActive(false);
+            return;
         }
 
-        imageFrame.Dispose();
-    }
-
-    void PositionLabel(Mediapipe.Tasks.Components.Containers.Detection detection)
-    {
-        RectTransform canvasRect = canvas.GetComponent<RectTransform>();
-        float canvasW = canvasRect.rect.width;
-        float canvasH = canvasRect.rect.height;
-
-        float faceCenterX;
-        float faceTopY;
-
-        if (detection.keypoints != null && detection.keypoints.Count >= 2)
+        if (_result.detections == null || _result.detections.Count == 0)
         {
-            // Keypoints: 0=right eye, 1=left eye, 2=nose, 3=mouth, 4=right ear, 5=left ear
-            var rightEye = detection.keypoints[0];
-            var leftEye = detection.keypoints[1];
-            faceCenterX = (rightEye.x + leftEye.x) / 2f;
-            faceTopY = Mathf.Min(rightEye.y, leftEye.y) - labelOffsetAboveHead;
-        }
-        else
-        {
-            // Fallback: bounding box
-            var box = detection.boundingBox;
-            faceCenterX = box.xMin + box.width / 2f;
-            faceTopY = box.yMin;
+            headLabel.gameObject.SetActive(false);
+            return;
         }
 
-        // Mirror X for selfie view, convert to canvas anchored position
-        float screenX = (1f - faceCenterX) * canvasW - canvasW / 2f;
-        float screenY = (1f - faceTopY) * canvasH - canvasH / 2f;
+        if (runner.ImageWidth <= 0 || runner.ImageHeight <= 0)
+            return;
 
-        headLabel.anchoredPosition = new Vector2(screenX, screenY);
-        headLabelText.text = "Hello!";
-    }
+        var detection = _result.detections[0];
+        var box = detection.boundingBox;
 
-    void OnDestroy()
-    {
-        if (_detector != null) _detector.Close();
-        if (_webcam != null && _webcam.isPlaying) _webcam.Stop();
-        if (_cpuTexture != null) Destroy(_cpuTexture);
+        float faceCenterX = (box.left + box.right) * 0.5f / runner.ImageWidth;
+        float faceTopY = box.top / (float)runner.ImageHeight;
+
+        if (mirrorX)
+            faceCenterX = 1f - faceCenterX;
+
+        float localX = (faceCenterX - 0.5f) * displayRect.rect.width;
+        float localY = (0.5f - faceTopY) * displayRect.rect.height + yOffset;
+
+        Vector3 targetWorldPos = displayRect.TransformPoint(new Vector3(localX, localY, 0f));
+
+        headLabel.position = Vector3.Lerp(
+            headLabel.position,
+            targetWorldPos,
+            1f - Mathf.Exp(-smoothing * Time.deltaTime)
+        );
+
+        headLabel.gameObject.SetActive(true);
+
+        if (headLabelText != null)
+            headLabelText.text = labelText;
     }
 }
